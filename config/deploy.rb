1993d814f3f61eca580ccd8612aa328cffe8c363
Daniel <fvcdaniel@gmail.com>
2013-06-24 16:36:55 -0300

    deploy cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,34 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "Feminissima Modas"
+set :repository,  "git://github.com/fvcdaniel/feminissima.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "feminissimamodas.com.br"                          # Your HTTP server, Apache/etc
+role :app, "feminissimamodas.com.br"                          # This may be the same as your `Web` server
+role :db,  "feminissimamodas.com.br", :primary => true # This is where Rails migrations will run
+role :db,  "feminissimamodas.com.br"
+
+default_run_options[:pty] = true  # Must be set for the password prompt
+                                  # from git to work
+set :scm, "git"
+set :user, "deployer"  # The server's user for deploys
+#set :scm_passphrase, "password"  # The deploy user's password
+set :ssh_options, { :forward_agent => true }
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+set :deploy_to, "/var/www/feminissimamodas.com.br"
+
+after :deploy, 'deploy:database'
+namespace :deploy do
+    task :database, :roles => :app do
+        #run "cd #{deploy_to}/current && bundle exec rake assets:precompile:all"
+        run "cp #{deploy_to}/shared/database.yml #{current_path}/config/"
+        run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec rake db:migrate"
+        run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec rake assets:precompile"
+    end
+end
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
