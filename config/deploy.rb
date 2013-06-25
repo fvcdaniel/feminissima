f7ab3a919dbaa0de5473f6c055be01e1fd95d38e
Daniel <fvcdaniel@gmail.com>
2013-06-25 01:06:19 -0300

    teste

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,8 +28,8 @@ namespace :deploy do
     task :database, :roles => :app do
     	run "cp #{deploy_to}/shared/database.yml #{current_path}/config/"
     	run "cp #{deploy_to}/shared/secret_token.rb #{current_path}/config/initializers/secret_token.rb"
-        #run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec rake db:migrate"
-        #run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec rake assets:precompile"
+        run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec rake db:migrate"
+        run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec rake assets:precompile"
     end
 end
 
